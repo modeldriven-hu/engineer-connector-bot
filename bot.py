1955203b7228101e895321dd91748a19433e737d
Zsolt Sandor <zsolt.sandor@gmail.com>
2023-02-12 18:56:09 +0100

    The bot is now able to send a response

--- bot.py
+++ bot.py
@@ -6,12 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-print(TOKEN)
-
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    response = "Hello world from python"
+    await message.channel.send(response)
 
 client.run(TOKEN)
