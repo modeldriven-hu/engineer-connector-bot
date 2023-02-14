9c496d8a40361b4aab756d780f2e2022c1aec280
Elias Johan Liavaag <elias.johan.liavaag@gmail.com>
2023-02-14 08:44:49 +0100

    Added a ping command, and default arguments for set_location

--- bot.py
+++ bot.py
@@ -12,17 +12,27 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
+@bot.command(name="ping", help="Pings the bot")  # for debugging purpose
+async def ping(ctx: Context):
+    username = str(ctx.author).split("#")[0]
+    print(f'Pinged by {username}')
+    await ctx.send('Pong!')
+
 @bot.command(name="set_location", help="Set the location")
-async def set_location(ctx: Context):
+async def set_location(ctx: Context, city="Helm's Deep", country="Rohan"):
     command = ctx.message.clean_content
     print(ctx.current_argument)
     print(ctx.current_parameter)
-    city = 'Budapest'
-    country = 'Hungary'
-    await ctx.send("Location for user " + ctx.author.name + " set to " + city + "," + country)
+    username = str(ctx.author).split("#")[0]
+    city = city.replace(',', '').replace('.', '')
+    country = country.replace(',', '').replace('.', '')
+    print("Location for user " + ctx.author.name + " set to " + city + ", " + country)
+    await ctx.send("Location for user " + ctx.author.name + " set to " + city + ", " + country)
 
 @bot.command(name="remove_location", help="Remove the location")
 async def remove_location(ctx: Context):
+    username = str(ctx.author).split("#")[0]
+    print("Location removed for " + ctx.author.name)
     await ctx.send("Location removed for " + ctx.author.name)
 
 bot.run(TOKEN)
